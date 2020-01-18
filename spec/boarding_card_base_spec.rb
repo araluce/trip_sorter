83a19a58bacbd25de0204e52f04f9996652fc2b1
Álvaro Fernández-Alonso Araluce <alvaro.fernandez@nazaries.com>
2020-01-18 12:13:59 +0100

    Test: Boarding card spec

---
+++ spec/boarding_card_base_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'boarding_card/base'
+
+describe BoardingCard::Base do
+
+  describe 'to_s' do
+
+    it 'should show the boarding card route' do
+      boarding_card = BoardingCard::Base.new('78A', 'Madrid', 'Barcelona', '45B')
+
+      expect(boarding_card.to_s).to eq('Madrid - Barcelona')
+    end
+
+    it 'should show the seat number text' do
+      boarding_card = BoardingCard::Base.new('78A', 'Madrid', 'Barcelona', '45B')
+
+      expect(boarding_card.seat_to_s).to eq('Sit in seat 45B')
+    end
+
+    it 'should show a free seat number' do
+      boarding_card = BoardingCard::Base.new('78A', 'Madrid', 'Barcelona', nil)
+
+      expect(boarding_card.seat_to_s).to eq('No  seat  assignment')
+    end
+  end
+
+end
